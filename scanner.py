523fc78850328ddb7d12d95c0532de2eb21ea11d
kalv-aro <alvarobalada73@gmail.com>
2023-01-03 21:51:34 +0100

    First commit

---
+++ scanner.py
@@ -0,0 +1,23 @@
+import subprocess
+import netifaces
+
+def net_scan():
+
+    
+    
+
+
+    
+    
+
+    
+    for address in network:
+
+        r = subprocess.call(['ping', '-c', "1", address])
+
+        if r == 0: 
+            print()
+
+
+
+    
